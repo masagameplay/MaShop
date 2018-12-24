5d446f7bbb9083f3185dd830b96994fd31c22ca1
Masa <masa@simasasolutions.com>
2018-12-24 18:14:54 +0200

    Initial commit

---
+++ src/main/java/fi/matiaspaavilainen/mashop/MaShop.java
@@ -0,0 +1,18 @@
+package fi.matiaspaavilainen.mashop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MaShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
